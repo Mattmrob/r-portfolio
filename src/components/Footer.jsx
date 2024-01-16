e392df58cdedcfee9135a34917bf1fef896fc582
Mysmelf <matthewr9937@gmail.com>
2024-01-16 15:39:12 -0800

    Added icons and links to footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,17 @@ export default function Footer() {
   return (
         <div className='footer'>
 
-            <a className="footerlink" target='_blank' href="https://github.com/Mattmrob">My github Profile</a>
+            <a className="footerlink" target='_blank' href="https://github.com/Mattmrob">
+              <img className="footerlogo" src="./src/assets/imgs/github-logo.svg" alt="a link to my github profile" />
+            </a>
 
-            <a className="footerlink" target='_blank' href="https://github.com/Mattmrob">My github Profile</a>
+            <a className="footerlink" target='_blank' href="https://github.com/Mattmrob">
+             <img className="footerlogo" src="./src/assets/imgs/linkedin-logo.svg" alt="a link to my linkedin profile" />
+            </a>
 
-            <a className="footerlink" target='_blank' href="https://github.com/Mattmrob">My github Profile</a>
+            <a className="footerlink" target='_blank' href="https://mmroberts.com/index.html">
+            <img className="footerlogo" src="./src/assets/imgs/mmrobertslogo.svg" alt="a link to mmroberts.com" />
+            </a>
 
         </div>
   );
