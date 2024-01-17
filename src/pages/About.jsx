0185a42e06a4db73994dad6fa0bca513aed0314c
Mysmelf <matthewr9937@gmail.com>
2024-01-16 16:57:57 -0800

    Began adding the About Me section, have profile image and some starting text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,24 @@
 export default function AboutPage() {
     return (
-      <div className="container pt-4">
-        <h1>Content of the About Page</h1>
+
+      <div className="container-fluid pt-4">
+
+        <div className='row align-center'>
+
+          <div className="col-3 test d-flex justify-content-center">
+            <img className="profileimg" src="./src/assets/imgs/profileimg.jpg" alt="A picture of myself" />
+          </div>
+
+          <div className="d-flex flex-column justify-content-center col-9">
+            <h1 className="title">About Me</h1>
+            <p className="bodytxt">
+              My name is Matthew Michael Roberts and I am a creative with expertise in Full-stack Web Developement and Graphic Design.
+              
+            </p>
+          </div>
+
+        </div>
+
       </div>
     );
   }
