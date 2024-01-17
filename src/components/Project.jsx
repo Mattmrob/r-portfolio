979d8015febf5f96b3909aecbe02d9638e3aa8ac
Mysmelf <matthewr9937@gmail.com>
2024-01-17 15:01:59 -0800

    Continued dev on projects page css.. working on mobile responsiveness now

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,9 +3,8 @@ function Project(props) {
       <ul className="p-project">
         {props.project.map((project) => {
             return(
-          <div key={project.id}>
+          <div class="img-container" key={project.id}>
 
-            <div className="img-container">
 
             <img
             alt={project.title}
@@ -18,14 +17,13 @@ function Project(props) {
                 <h3>{project.title}</h3>
                 <p>{project.text}</p>
 
-                <a target="_blank" href={project.github}>Github</a>
-                <a target="_blank" href={project.deployment}>Deployment</a>
+                <p className="projectlink"><a target="_blank" href={project.github}>Github</a>
+                <a target="_blank" href={project.deployment}>Deployment</a></p>
 
             </div>
 
             </div>
-          </div>
-            );
+          );
         })}
       </ul>
 
