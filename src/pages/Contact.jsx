37c8b06a13916cd21f18368be50d1e3b4007f0bf
Mysmelf <matthewr9937@gmail.com>
2024-01-17 18:20:20 -0800

    Began working on react forum functionality

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,25 @@
+import { useState } from 'react';
+import { validateEmail } from '../utils/helpers.js';
+
+const [email, setEmail] = useState('');
+const [name, setName] = useState('');
+const [text, setText] = useState('');
+
+const handleInputChange = (e) => {
+  const { target } = e;
+  const inputType = target.name;
+  const inputValue = target.value;
+
+  if (inputType === 'email') {
+    setEmail(inputValue);
+  } else if (inputType === 'name') {
+    setName(inputValue);
+  } else if (inputType === 'text') {
+    setText(inputValue);
+  }
+};
+
+
 export default function ContactPage() {
     return (
       <div className="container pt-4">
@@ -9,13 +31,13 @@ export default function ContactPage() {
           <h2>I am best reached through email or phone call!</h2>
           <h3>matthew@mmroberts.com • 925-350-1818 • mmroberts.com</h3>
 
-          <input>
+          <input id="name">
           </input>
 
-          <input>
+          <input id="email">
           </input>
 
-          <input>
+          <input id="text">
           </input>
 
           <button>
